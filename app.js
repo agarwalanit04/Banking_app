b10f0076accbe12524556388c26020789ffad1a4
Anit Agarwal <126401607+agarwalanit04@users.noreply.github.com>
2024-06-28 15:20:58 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -1,38 +1,39 @@
 const express = require("express");
 const mongoose = require('mongoose');
 const app = express();
+
 // Routes
 const accountRouter = require('./routers/accountRouter');
-// const customerRouter = require('./routers/customerRouter');
+const customerRouter = require('./routers/customerRouter');
 const transactionRouter = require('./routers/transactionRouter');
-// const userRouter = require('./routers/userRouter');
+const userRouter = require('./routers/userRouter');
 
 // MongoDB connection URL and Database Name
 const dbName = 'Banking';
 const accountscollection = 'accounts_table';
+
 // Middleware to parse JSON bodies
 app.use(express.json());
 
 async function connectToMongoDB(req, res, next) {
   try {
-       const url = `mongodb+srv://${process.env.MONGODB_URL}/Banking?retryWrites=true&w=majority&appName=Cluster0`;
-       console.log(url);
-       mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
-       .then(() => {
-         console.log('MongoDB connected');
-         next()
-       }).catch(err => console.error(err));
+    const url = `mongodb+srv://${process.env.MONGODB_URL}/Banking?retryWrites=true&w=majority&appName=Cluster0`;
+    console.log(url);
+    mongoose.connect(url)
+      .then(mongoClient => {
+        console.log('MongoDB connected');
+        next();
+      })
+      .catch(err => console.error(err));
   } catch (err) {
-      console.error("Failed to connect to MongoDB", err);
-      res.status(500).send({message:"internal server error"});
+    console.error("Failed to connect to MongoDB", err);
+    res.status(500).send({ message: "internal server error" });
   }
- }
- 
+}
 app.use(connectToMongoDB);
 
-
 app.use('/account', accountRouter);
-// app.use('/customer', customerRouter);
+app.use('/customer', customerRouter);
 app.use('/transaction', transactionRouter);
 // app.use('/user', userRouter);
 
@@ -42,5 +43,5 @@ app.use('/transaction', transactionRouter);
 
 
 
-
 module.exports.app=app;
+
--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const {app} = require("./app")
+
 const port=3000;
 require('dotenv').config();
 
@@ -6,4 +7,6 @@ require('dotenv').config();
 // Start the server
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
+
 });
+
