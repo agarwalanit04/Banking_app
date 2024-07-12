fb45d5c2753732d8ec7a19199892fa7513b467a5
Anit Agarwal <126401607+agarwalanit04@users.noreply.github.com>
2024-07-13 00:03:21 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -1,7 +1,26 @@
+const express = require('express');
+const cors = require('cors');
+// const app = express();
 const {app} = require("./app")
 
 const port=3000;
 require('dotenv').config();
+
+const corsOptions = {
+    origin: ['*'],
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    allowedHeaders: ['Content-Type', 'Authorization']
+};
+
+app.use(cors(corsOptions));
+
+app.get('/your-api-endpoint', (req, res) => {
+    res.json({ message: 'Hello from the API' });
+});
+
+// app.listen(3000, () => {
+//     console.log('Server is running on port 3000');
+// });
 
 
 // Start the server
