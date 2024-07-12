80a62add8a4682e7e84d5bc49d1ae00e7248c5ed
Anit Agarwal <126401607+agarwalanit04@users.noreply.github.com>
2024-07-12 23:01:58 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -1,41 +1,42 @@
-const express = require("express");
-const mongoose = require('mongoose');
-const app = express();
+  const express = require("express");
+  const mongoose = require('mongoose');
+  const app = express();
+  // require('dotenv').config();
 
-// Routes
-const accountRouter = require('./routers/accountRouter');
-const customerRouter = require('./routers/customerRouter');
-const transactionRouter = require('./routers/transactionRouter');
-const userRouter = require('./routers/userRouter');
+  // Routes
+  const accountRouter = require('./routers/accountRouter');
+  const customerRouter = require('./routers/customerRouter');
+  const transactionRouter = require('./routers/transactionRouter');
+  const userRouter = require('./routers/userRouter');
 
-// MongoDB connection URL and Database Name
-const dbName = 'Banking';
-const accountscollection = 'accounts_table';
+  // MongoDB connection URL and Database Name
+  const dbName = 'Banking';
+  const accountscollection = 'accounts_table';
 
-// Middleware to parse JSON bodies
-app.use(express.json());
+  // Middleware to parse JSON bodies
+  app.use(express.json());
 
-async function connectToMongoDB(req, res, next) {
-  try {
-    const url = `mongodb+srv://${process.env.MONGODB_URL}/Banking?retryWrites=true&w=majority&appName=Cluster0`;
-    console.log(url);
-    mongoose.connect(url)
-      .then(mongoClient => {
-        console.log('MongoDB connected');
-        next();
-      })
-      .catch(err => console.error(err));
-  } catch (err) {
-    console.error("Failed to connect to MongoDB", err);
-    res.status(500).send({ message: "internal server error" });
+  async function connectToMongoDB(req, res, next) {
+    try {
+      const url = `mongodb+srv://${process.env.MONGODB_URL}/Banking?retryWrites=true&w=majority&appName=Cluster0`;
+      console.log(url);
+      mongoose.connect(url)
+        .then(mongoClient => {
+          console.log('MongoDB connected');
+          next();
+        })
+        .catch(err => console.error(err));
+    } catch (err) {
+      console.error("Failed to connect to MongoDB", err);
+      res.status(500).send({ message: "internal server error" });
+    }
   }
-}
-app.use(connectToMongoDB);
+  app.use(connectToMongoDB);
 
-app.use('/account', accountRouter);
-app.use('/customer', customerRouter);
-app.use('/transaction', transactionRouter);
-// app.use('/user', userRouter);
+  app.use('/account', accountRouter);
+  app.use('/customer', customerRouter);
+  app.use('/transaction', transactionRouter);
+  app.use('/user', userRouter);
 
 
 //GET http://localhost:3000/user/balance/12334
